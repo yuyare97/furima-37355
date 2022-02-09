6e250b960692efa5888d3ef9357c9f80c12ea67f
yuyare97 <yuya.take1020@gmail.com>
2022-02-09 12:54:16 +0900

    ログイン、新規登録、表示分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
 end
