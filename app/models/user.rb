cc7b3524bdad96fa42a7de84790af8794b5b4b1b
yuyare97 <yuya.take1020@gmail.com>
2022-02-10 14:20:30 +0900

    テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+  has_many :orders
 
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
 
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}, presence: true
+
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}, presence: true
+
+  validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ン]+\z/}, presence: true
 end
