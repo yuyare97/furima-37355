391b49a812ffd0f6d41a13a1bf3a1321b2390121
yuyare97 <yuya.take1020@gmail.com>
2022-02-10 23:56:45 +0900

    商品出品ページの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delv_days
+
+  validates :image, :name, :description, :price, presence: true
+  validates :category_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :burden_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :scheduled_day_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :price, numericality: { greater_than: 299, less_than: 10_000_000, message: "can't be blank" }
+  validates :price, format: { with: /[0-9]+\d/}, presence: true
 end
