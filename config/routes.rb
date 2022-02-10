60b5e3daf0d0d52dab89f8859b5cfa3fa2008711
yuyare97 <yuya.take1020@gmail.com>
2022-02-10 19:06:18 +0900

    モデルとテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'furima/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
+
+  resources :items, only: [:index, :new]
 end
