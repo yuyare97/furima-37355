4dbbc2d73c824d24d5a6e6a2340d208f60c4f147
yuyare97 <yuya.take1020@gmail.com>
2022-02-11 00:18:31 +0900

    creatアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,13 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
