25249e2a70f13031c9688c8f1b39e37e4547fb62
yuyare97 <yuya.take1020@gmail.com>
2022-02-11 13:05:16 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentence }
+    price { 1000 }
+    category_id { 1 }
+    condition_id { 1 }
+    burden_id { 1 }
+    prefecture_id { 1 }
+    scheduled_day_id { 1 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'aaa111'}
+    nickname              { Faker::Name.first_name }
+    email                 { Faker::Internet.free_email }
+    password              { '1Aa' + Faker::Internet.password(min_length: 6, mix_case: true) }
     password_confirmation {password}
     last_name             {'青山'}
     first_name            {'太郎'}
     last_name_kana        {'アオヤマ'}
     first_name_kana       {'タロウ'}
-    birthday              {'1930-01-01'}
+    birthday              { Faker::Date.birthday(min_age: 12, max_age: 70) }
   end
 end
