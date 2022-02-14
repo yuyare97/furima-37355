5b86559489680a9774239c89011b8c29ef1caafb
yuyare97 <yuya.take1020@gmail.com>
2022-02-14 21:19:28 +0900

    updateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
