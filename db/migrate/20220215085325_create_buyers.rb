f90441c7457ce05cd30c278f9fdba510b6bce7ac
yuyare97 <yuya.take1020@gmail.com>
2022-02-15 18:02:24 +0900

    カラム作成

--- db/migrate/20220215085325_create_buyers.rb
+++ db/migrate/20220215085325_create_buyers.rb
@@ -1,7 +1,13 @@
 class CreateBuyers < ActiveRecord::Migration[6.0]
   def change
     create_table :buyers do |t|
-
+      t.string     :postcode,          null: false
+      t.integer     :prefecture_id,    null: false
+      t.string     :city,              null: false
+      t.string     :address,           null: false
+      t.string     :apartment_name
+      t.string     :phone_number,      null: false
+      t.references :order,             null: false, foreign_key: true
       t.timestamps
     end
   end
