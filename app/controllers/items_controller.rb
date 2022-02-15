bd8f14d997473c232bfc3fc90a8a4c7b33088072
yuyare97 <yuya.take1020@gmail.com>
2022-02-15 12:36:30 +0900

    Revert "Merge pull request #17 from yuyare97/商品情報編集機能"
    This reverts commit 6153377ebdd01db5ddae9c9c0b54ec58c88c6a13, reversing
    changes made to c2f322a1356b717b3e06d3082e25621d6c9ea8ba.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, only: [:new]
+
 
   def index
     @items = Item.order(created_at: "DESC")
@@ -21,30 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(@item.id)
-    else
-      render :edit
-    end
+    @item = Item.find(params[:id])
   end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :burden_id, :prefecture_id, :scheduled_day_id, :price).merge(user_id: current_user.id)
-  end
-
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
-  def move_to_index
-    redirect_to root_path unless current_user == @item.user
   end
 end
