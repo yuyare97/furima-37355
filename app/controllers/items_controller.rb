6873553095617ba281741ffca769bfb993aa1b7d
yuyare97 <yuya.take1020@gmail.com>
2022-02-15 12:01:34 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :authenticate_user!, only: [:new]
   before_action :move_to_index, only: [:edit]
 
   def index
