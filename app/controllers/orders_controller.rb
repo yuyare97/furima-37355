de0cae4d9a284683b94a3584c27a6576de1a8b2e
yuyare97 <yuya.take1020@gmail.com>
2022-02-16 20:57:34 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,11 +39,11 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price,  
+      card: order_params[:token],   
+      currency: 'jpy'                 
     )
   end
 
--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -2,14 +2,15 @@ class OrderBuyer
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :address, :apartment_name, :phone_number, :token
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
+  with_options presence: true do
+  validates :user_id
+  validates :item_id
   validates :postcode, format: { with: /\A\d{3}-\d{4}\z/, message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
-  validates :city, presence: true
-  validates :address, presence: true
+  validates :city
+  validates :address
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "can't be blank"}
-  validates :token, presence: true
+  validates :token
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
