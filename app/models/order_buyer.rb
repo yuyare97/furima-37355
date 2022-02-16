fd1fb153183f36b89a003ff41a44f83fe925f9da
yuyare97 <yuya.take1020@gmail.com>
2022-02-16 20:42:57 +0900

    修正

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -2,14 +2,14 @@ class OrderBuyer
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :address, :apartment_name, :phone_number, :token
 
-  validate :user_id
-  validate :item_id
+  validates :user_id, presence: true
+  validates :item_id, presence: true
   validates :postcode, format: { with: /\A\d{3}-\d{4}\z/, message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
   validates :city, presence: true
   validates :address, presence: true
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "can't be blank"}
-  validate :token
+  validates :token, presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
