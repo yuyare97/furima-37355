b5adc4b92a8c079cb85ce236599ab271f4b19edc
yuyare97 <yuya.take1020@gmail.com>
2022-02-16 14:52:12 +0900

    購入ページ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index]
 
   def index
-    @item = Item.find(params[:item_id])
     @order = OrderBuyer.new
   end
 
   def create
     @order = OrderBuyer.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
 
@@ -16,6 +22,12 @@ class OrdersController < ApplicationController
     params.require(:order_buyer).permit(:postcode, :prefecture_id, :city, :address, :apartment_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 
 end
--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -12,7 +12,7 @@ class OrderBuyer
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, apartment_name: apartment_name,
+    Buyer.create(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, apartment_name: apartment_name,
     phone_number: phone_number, order_id: order.id)
   end
 
